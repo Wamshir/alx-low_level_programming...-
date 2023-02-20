25e86e23c20afb4d745de173905de162f0e7b7ac
Wamshir <clintonshiroya@gmail.com>
2023-02-20 15:51:35 -0800

    Trial

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**                                                * main - Entry
+ * Description - 'Task9'                           * Return: Always0
+ */                                               int main(void)
+{
+        int n;
+                                                          for (n = 48; n <=57 ; n++)
+        {
+                putchar(n);
+                if (n == 57)
+                {
+                        continue;
+                }
+                putchar (',');
+                putchar (',');
+        }
+        putchar ('\n');                                   return (0);
+}
