681be751dce1046ab8dc0c44b1edd3b08c92e44a
Wamshir <clintonshiroya@gmail.com>
2023-02-20 16:22:30 -0800

    rush

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - Entry
+ * Desctiotion - Hay
+ * Return: Always 
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
