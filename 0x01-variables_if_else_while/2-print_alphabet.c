66cc7ec8bec559648109322c656fa5e7e00f7bea
Wamshir <clintonshiroya@gmail.com>
2023-02-22 12:15:57 -0800

    yas

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - Entry Point
+ * Description - 'Print Lower case'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+variableifelseread
