d43f93a5e74ef4e5037a8113731a8b63abf8ac05
Wamshir <122922619+Wamshir@users.noreply.github.com>
2023-02-23 02:15:51 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"                        /**
+ * 6-abs.c - Absolute Value
+ * t - integer to be computed
+ * Return: Absolute Value
+ */
+int _abs(int t)
+{
+        if (t >= 0)
+                return (t);
+        else
+                return (-t);
+}
