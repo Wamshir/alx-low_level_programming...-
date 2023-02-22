b81186fcb0cd8cb6f154509b214dd533d003dce4
Wamshir <122922619+Wamshir@users.noreply.github.com>
2023-02-23 02:59:24 +0300

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include "main.h"                        /**                                       * jack_bauer - prints every minute of the day
+ */                                      void jack_bauer(void)
+{
+        int hour, minute;                        for (hour = 0; minute <= 59; minu
+te ++)
+        {                                                for (minute = 0, minute <
+= 59, minute ++)
+                {                                                _putchar((hour /
+10) + '0');                                                      _putchar((hour %
+10) + '0');                                                      _putchar(':');                           _putchar((minute
+/ 10) + '0');                                                    _putchar((minute
+% 10) + '0');
+                        _putchar('\n');                  }
