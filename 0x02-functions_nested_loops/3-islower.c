f3ce1c6f3819cdbb447bab0523ecaafd3c47af9f
Wamshir <122922619+Wamshir@users.noreply.github.com>
2023-02-23 01:56:11 +0300

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"                      
+/**                                       
+* _islower - checks the lower characters
+ * @c : the lower character             
+ * Return: 1 if letter is lower case; 0 if not
+ */
+void _islower(int c)
+{                                                if (c >= 'a' && c <= 'z')
+                return (1);
+        else                                             return (0);
+}
